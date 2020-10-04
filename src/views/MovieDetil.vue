cbffd7aa48acef52326034e9159644af23b63baa
“SeiyaHirata” <s-hirata@g-account.page>
2020-10-04 09:49:44 +0000

    routerサンプル

---
+++ src/views/MovieDetil.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>詳細画面</h1>
+    <h1>ID:{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieDetil",
+  components: {},
+  data() {
+    return {
+      id: this.$route.params.id
+    };
+  }
+};
+</script>
---
+++ src/views/SearchMovies.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>検索一覧</h1>
+    <v-slider
+      v-model="id"
+      label="thumb-color"
+      thumb-color="red"
+      thumb-label="always"
+    ></v-slider>
+    <v-btn @click="MovieDetil" color="primary">詳細へ{{ id }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchMovies",
+  components: {},
+  data() {
+    return {
+      id: ""
+    };
+  },
+  methods: {
+    MovieDetil() {
+      this.$router.push({ name: "MovieDetil", params: { id: this.id } });
+    }
+  }
+};
+</script>
