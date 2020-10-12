d7a603b5c5a9d2c25c18b8d849b74b7fb3c6e999
daisuke818 <puyo.puyo456@icloud.com>
2020-10-12 12:40:34 +0900

    step1　エラーの修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/MovieDetil.vue";
+import MovieDetil from "../views/MovieDetil.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "MovieDetil",
+    component: MovieDetil
   },
   {
     path: "/about",
