0ce47b89bab572811faace0f8995849f22edf9d8
GtiHubãshinshin4n4n <s.iguchi777@gmail.com>
2020-10-26 11:25:57 +0000

    課題１－１～１－３

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
+        <v-imgs
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
@@ -10,7 +10,6 @@
           transition="scale-transition"
           width="40"
         />
-
         <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
@@ -20,9 +19,7 @@
           width="100"
         />
       </div>
-
       <v-spacer></v-spacer>
-
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
@@ -34,23 +31,26 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld
-  },
+  components: {},
 
   data: () => ({
     //
-  })
+  }),
+  methods: {
+    toSearchMovies() {
+      this.$router.push({ name: "SearchMovies" });
+    }
+  }
 };
 </script>
